6a3bbf26f1340e6933d607c0898b2f39368ef829
IGNIS\admin <tuliodnw@gmail.com>
2022-11-14 10:44:29 +0100

    changes

--- uebung3.pl
+++ uebung3.pl
@@ -1,4 +1,3 @@
-
 %1
 kante_gg(a, b).
 kante_gg(a, e).
@@ -23,17 +22,16 @@ pfad_gg(X,Y):-kante_gg(X, Z), pfad_gg(Z,Y).
 
 %3.0
 %3.1
-
 %pfad_gg2(g,c,P). P = [g, h, f, c].
 pfad_gg2(X,Y,[X,Y]):-kante_gg(X,Y).
 pfad_gg2(X,Y,[X|P]):-kante_gg(X,Z), pfad_gg2(Z,Y,P).
 
 %3.2
-
 %pfad_gg3(g,c,[g],P). P = [c, f, h, g]
 %pfad_gg3(a,c,[a],P). P = [c, b, a] ; P = [c, f, b, a] ;P = [c, f, e, a] ;
 pfad_gg3(X,Y,A,[Y|A]):- kante_gg(X,Y).
-pfad_gg3(X,Y,A,P):- kante_gg(X,Z),not(member(Z,A)),pfad_gg3(Z,Y,[Z|A],P).
+%pfad_gg3(X,Y,A,P):- kante_gg(X,Z),not(member(Z,A)),pfad_gg3(Z,Y,[Z|A],P).
+pfad_gg3(X,Y,A,P):- kante_gg(X,Z),pfad_gg3(Z,Y,[Z|A],P).
 
 %4
 %4.1
@@ -53,10 +51,8 @@ kante_zg(g, h).
 kante_zg(h, f).
 
 pfadz(X,Y,[X|R],[Y|[X|R]]):-kante_zg(X,Y).
-pfadz(X,Y,[X|R],P):-kante_zg(X, NEW),not(member(NEW,R)),pfadz(NEW,Y,[NEW|[X|R]],P).
-
+pfadz(X,Y,A,P):- kante_zg(X,Z),not(member(Z,A)),pfadz(Z,Y,[Z|A],P). %Easier
 %4.2
-
 
 %5
 kante_ug(b, o).
@@ -74,16 +70,16 @@ pfad_ug(_,Y,[LAST|R],[Y|[LAST|R]]):-connection(Y,LAST).
 pfad_ug(X,Y,[X|R],P):-connection(X, NEW),not(NEW==Y),not(member(NEW,R)),pfad_ug(NEW,Y,[NEW|[X|R]],P).
 
 %5.1
-nb(bayern, thüringen).
-nb(bayern, badenwürttemberg).
+nb(bayern, thueringen).
+nb(bayern, badenwuerttemberg).
 nb(bayern, sachsen).
 nb(bayern, hessen).
-nb(badenwürttemberg, hessen).
-nb(badenwürttemberg, rheinlandpfalz).
+nb(badenwuerttemberg, hessen).
+nb(badenwuerttemberg, rheinlandpfalz).
 nb(saarland, rheinlandpfalz).
-nb(hessen, thüringen).
-nb(sachsen, thüringen).
-nb(sachsenanhalt, thüringen).
+nb(hessen, thueringen).
+nb(sachsen, thueringen).
+nb(sachsenanhalt, thueringen).
 nb(hessen, niedersachsen).
 nb(niedersachsen, nordrheinwestfalen).
 nb(hessen, nordrheinwestfalen).
@@ -100,9 +96,11 @@ nb(mecklenburgvorpommern, niedersachsen).
 nb(mecklenburgvorpommern, schleswigholstein).
 nb(hamburg, schleswigholstein).
 
+
 pp(X,Y):-nb(X,Y);nb(Y,X).
-dbr(_,Y,[LAST|R],[Y|[LAST|R]]):-pp(Y,LAST).
-dbr(X,Y,[X|R],P):-pp(X, NEW),not(NEW==Y),not(member(NEW,R)),dbr(NEW,Y,[NEW|[X|R]],P).
+dbr(X,Y,[X|R],[Y|[X|R]]):-pp(X,Y).
+dbr(X,Y,A,P):- kante_zg(X,Z),not(member(Z,A)),dbr(Z,Y,[Z|A],P).
+
 
 %5.2
 
